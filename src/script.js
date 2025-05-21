f48af782c84d070b843d8f41f7233054aa136775
amir <amirtaki623@gmail.com>
2025-05-21 22:58:09 +0330

    js : edit on mouse move function

--- src/script.js
+++ src/script.js
@@ -7,7 +7,16 @@ let imgWrap = document.querySelector('.img-wrap')
 
 var leftSpace = imgBox.offsetLeft;
 
-imgBox.addEventListener("mouseover", (event)=> {
-    let boxWidth = (event.pageX - leftSpace) + "px"
+// imgBox.addEventListener("mousemove", (event)=> {
+//     let boxWidth = (event.pageX - leftSpace) + "px"
+//     imgWrap.computedStyleMap.width = boxWidth;
+//     console.log(boxWidth)
+// })
+
+
+imgBox.onmousemove = function(e){
+    var boxWidth = (e.pageX - leftSpace) + "px"
+    imgWrap.style.width = boxWidth
     console.log(boxWidth)
-})
+}
+// onmousemove()
