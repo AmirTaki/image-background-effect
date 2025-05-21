e36ae2a09d4930996986e7ff23529f2e52f176c1
amir <amirtaki623@gmail.com>
2025-05-21 23:18:40 +0330

    js : setting line witdth & arrow width

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 let imgBox  = document.querySelector('.img-box')
 let imgWrap = document.querySelector('.img-wrap')
 let orginalImg = document.querySelector('#orginalImg')
+let line = document.querySelector('#line')
 
 orginalImg.style.width = imgBox.offsetWidth + "px"
 
@@ -12,7 +13,9 @@ var leftSpace = imgBox.offsetLeft;
 
 
 imgBox.onmousemove = function(e){
-    var boxWidth = (e.pageX - leftSpace) + "px"
+    var boxWidth = (e.pageX - leftSpace ) + "px"
+    var widthArrow = e.pageX + "px"
     imgWrap.style.width = boxWidth
-    console.log(boxWidth)
+    line.style.left = widthArrow  
+
 }
